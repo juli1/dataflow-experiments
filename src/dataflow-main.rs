cea903648a4512aca78a1cba20e02a68560d93e6
Julien Delange <julien.delange@datadoghq.com>
2024-07-10 23:59:03 -0400

    push naive

--- src/dataflow-main.rs
+++ src/dataflow-main.rs
@@ -113,11 +113,11 @@ fn main() {
         std::process::exit(1);
     }
     let files_in_repository = get_files(args[1].as_str()).expect("");
-    let java_files = files_in_repository.iter().filter(|f| match_extension(f, vec!["java".to_string()]));
+    let java_files: Vec<PathBuf> = files_in_repository.into_iter().filter(|f| match_extension(f, vec!["java".to_string()])).collect();
     let mut parser = Parser::new();
     parser.set_language(&tree_sitter_java::language()).expect("error while loading Java language");
     let mut total_duration = Duration::new(0, 0);
-    for f in java_files {
+    for f in &java_files {
 
         // read filename into a string
         let source_code_res = std::fs::read_to_string(f);
@@ -137,5 +137,6 @@ fn main() {
     }
 
 
-    println!("total time: {} secs", total_duration.as_secs())
+    println!("total time: {} secs", total_duration.as_secs());
+    println!("number of files: {}", java_files.len());
 }
